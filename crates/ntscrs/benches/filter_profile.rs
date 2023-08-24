10137dd400918c54d82d9cfa4d8ef70645339b3e
valadaptive <valadaptive@protonmail.com>
2023-08-24 13:19:19 -0400

    Make benches work on Windows

--- crates/ntscrs/benches/filter_profile.rs
+++ crates/ntscrs/benches/filter_profile.rs
@@ -2,6 +2,7 @@ extern crate criterion;
 use criterion::{criterion_group, criterion_main, Criterion};
 use image::io::Reader as ImageReader;
 use ntscrs::ntsc::NtscEffect;
+#[cfg(not(target_os="windows"))]
 use pprof::criterion::{Output, PProfProfiler};
 
 fn criterion_benchmark(c: &mut Criterion) {
@@ -9,17 +10,24 @@ fn criterion_benchmark(c: &mut Criterion) {
         .unwrap()
         .decode()
         .unwrap();
-    let img = img.as_rgb8().unwrap();
+    let img = img.to_rgb8();
     c.bench_function("full effect", |b| {
         b.iter(|| {
-            NtscEffect::default().apply_effect(img, 0, 0);
+            NtscEffect::default().apply_effect(&img, 0, 0);
         })
     });
 }
 
 criterion_group! {
     name = benches;
-    config = Criterion::default().with_profiler(PProfProfiler::new(100, Output::Flamegraph(None)));
+    config = {
+        #[cfg(not(target_os="windows"))]
+        let config = Criterion::default().with_profiler(PProfProfiler::new(100, Output::Flamegraph(None)));
+        #[cfg(target_os="windows")]
+        let config = Criterion::default();
+
+        config
+    };
     targets = criterion_benchmark
 }
 criterion_main!(benches);
