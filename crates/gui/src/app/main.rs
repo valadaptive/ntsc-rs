58232417f7a9fff3df27ad2f1d0b17e5ead57fe1
valadaptive <valadaptive@protonmail.com>
2024-05-21 18:30:11 -0400

    Fix hiding command prompt when running

--- crates/gui/src/app/main.rs
+++ crates/gui/src/app/main.rs
@@ -1,5 +1,3 @@
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
-
 use std::{
     borrow::Cow,
     error::Error,
--- crates/gui/src/bin/ntsc-rs-standalone.rs
+++ crates/gui/src/bin/ntsc-rs-standalone.rs
@@ -1,3 +1,5 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+
 use std::error::Error;
 
 use gui::app::main::run;
