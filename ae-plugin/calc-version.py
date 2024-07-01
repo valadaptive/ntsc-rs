e852f79dd8902af94952dea07267efeec019841e
valadaptive <valadaptive@protonmail.com>
2024-07-01 17:18:18 -0400

    Remove C++ After Effects plugin
    And hopefully never look back. The Rust version should hopefully be
    easier to maintain and build.

--- ae-plugin/calc-version.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-version = sys.argv[1]
-version = [int(v) for v in version.split('.')]
-if len(version) != 3:
-    print('Invalid version format')
-    sys.exit(1)
-pipl_version = (version[0] << 19) | (version[1] << 15) | (version[2] << 11) | (3 << 9) | 1
-print(pipl_version)
