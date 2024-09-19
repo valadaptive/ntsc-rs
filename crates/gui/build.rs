499f96475ff17564b56871da16090cbf977eba21
valadaptive <valadaptive@protonmail.com>
2024-09-19 10:29:55 -0400

    Pass -headerpad_max_install_names on macOS
    This should hopefully make install_name_tool work

--- crates/gui/build.rs
+++ crates/gui/build.rs
@@ -5,4 +5,8 @@ fn main() {
     if env::var_os("CARGO_CFG_WINDOWS").is_some() {
         embed_resource::compile("icon.rc", embed_resource::NONE);
     }
+
+    if env::var("CARGO_CFG_TARGET_OS").is_ok_and(|os| os == "macos") {
+        println!("cargo:rustc-link-arg=-headerpad_max_install_names");
+    }
 }
