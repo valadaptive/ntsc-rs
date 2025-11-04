904a1976de448f59e14cb4a2e654bc81a5714d42
valadaptive <valadaptive@protonmail.com>
2025-11-04 12:28:22 -0500

    Batch the filter benchmark

--- crates/ntscrs/benches/filter_profile.rs
+++ crates/ntscrs/benches/filter_profile.rs
@@ -1,7 +1,12 @@
 extern crate criterion;
+use std::convert::identity;
+
 use criterion::{Criterion, black_box, criterion_group, criterion_main};
 use image::ImageReader;
-use ntscrs::{ntsc::NtscEffect, yiq_fielding::Rgb8};
+use ntscrs::{
+    ntsc::NtscEffect,
+    yiq_fielding::{BlitInfo, PixelFormat as _, Rgb8, YiqView},
+};
 #[cfg(not(target_os = "windows"))]
 use pprof::criterion::{Output, PProfProfiler};
 
@@ -12,19 +17,38 @@ fn criterion_benchmark(c: &mut Criterion) {
         .unwrap();
     let img = img.to_rgb8();
     c.bench_function("full effect", |b| {
-        b.iter(|| {
-            let img = img.clone();
-            let width = img.width() as usize;
-            let height = img.height() as usize;
-            let mut buf = img.into_raw();
-            NtscEffect::default().apply_effect_to_buffer::<Rgb8>(
-                (width, height),
-                &mut buf,
-                0,
-                [1.0, 1.0],
-            );
-            black_box(&mut buf);
-        })
+        b.iter_batched_ref(
+            || {
+                let img = img.clone();
+                let width = img.width() as usize;
+                let height = img.height() as usize;
+                let effect = NtscEffect::default();
+                let data = img.into_raw();
+                let scratch =
+                    vec![
+                        0f32;
+                        YiqView::buf_length_for((width, height), effect.use_field.to_yiq_field(0))
+                    ];
+
+                (width, height, effect, data, scratch)
+            },
+            |(width, height, effect, buf, scratch)| {
+                let mut yiq = YiqView::from_parts(
+                    scratch,
+                    (*width, *height),
+                    effect.use_field.to_yiq_field(0),
+                );
+                let row_bytes = *width * Rgb8::pixel_bytes();
+                yiq.set_from_strided_buffer::<Rgb8, _>(
+                    buf,
+                    BlitInfo::from_full_frame(*width, *height, row_bytes),
+                    identity,
+                );
+                effect.apply_effect_to_yiq(&mut yiq, 0, [1.0, 1.0]);
+                black_box(buf);
+            },
+            criterion::BatchSize::LargeInput,
+        );
     });
 }
 
